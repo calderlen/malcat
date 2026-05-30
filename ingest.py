cc71394c78830da3a943eaf3d9835e682081ff7e
Calder Lenhart <calderlen@gmail.com>
2026-05-30 14:09:01 -0400

    first commit

---
+++ ingest.py
@@ -0,0 +1,8 @@
+# interate through the light curve files
+# these are flat in the output bundles
+# each has a raw2 and dat2 -- raw2 can likely be neglected for now b/c the transformer may learn these features on its own -- dont want to overcode
+# so ingest the dat2 file csvs
+
+
+
+# now create tensors for the transformer model
